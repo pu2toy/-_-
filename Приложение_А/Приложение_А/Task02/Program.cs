49132823d5c56d748005be6c4a90fefe78644fd6
pu2toy <pu2toy1@gmail.com>
2026-03-16 12:17:00 +0300

    first commit

---
+++ Приложение_А/Приложение_А/Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите 4-значное число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        int result = (a / 1000) * (a / 100) * (a / 10) * (a % 10);
+        Console.WriteLine(result);
+    }
+}
