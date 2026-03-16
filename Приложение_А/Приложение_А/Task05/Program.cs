2d587db7f3065b1a773e717444762d86f4ce2048
pu2toy <pu2toy1@gmail.com>
2026-03-16 13:36:53 +0300

    Task05up

---
+++ Приложение_А/Приложение_А/Task05/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Вычисление объёма цилиндра\nВведите исходные данные:\nРадиус основания(см) ->");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Высота цилиндра(см) ->");
+        double height = Convert.ToDouble(Console.ReadLine());
+        double V = Math.PI * Math.Pow(radius, 2) * height;
+        Console.Write($"Объём цилиндра {V:N3}");
+    }
+}
