a77a4af512c781a6f65228ce3eb3b9904a891918
pu2toy <pu2toy1@gmail.com>
2026-03-16 13:54:54 +0300

    addTask060708

---
+++ Приложение_А/Приложение_А/Task07/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите a: ");
+        double a = Convert.ToDouble(Console.Read());
+        double z1 = (1 - 2 * Math.Pow(Math.Sin(a), 2)) / (1 + Math.Sin(2 * a));
+        double z2 = (1 - Math.Tan(a)) / (1 + Math.Tan(a));
+        Console.WriteLine($"z1 = {z1:N2}\nz2 = {z2:N2}");
+    }
+}
