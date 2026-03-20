34a3d965d372fc87975037d398c5a8491d7ca7e8
pu2toy <pu2toy1@gmail.com>
2026-03-20 11:39:31 +0300

    Added 3 tasks

---
+++ 4_Методы._Реализация полиморфизма/Topic_4/Task08/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        double f;
+        ConvertCelsiumToFahrenheit(25, out f);
+        Console.WriteLine(f + "°F");
+        ConvertKelvinToFahrenheit(273, out f);
+        Console.WriteLine(Math.Round(f, 7) + "°F");
+    }
+
+    static void ConvertCelsiumToFahrenheit(in double celsius, out double fahrenheit)
+    {
+        fahrenheit = (celsius * (9 / 5)) + 32;
+    }
+
+    static void ConvertKelvinToFahrenheit(in double kelvin, out double fahrenheit)
+    {
+        fahrenheit = ((kelvin - 273.15) * (9 / 5)) + 32;
+    }
+}
