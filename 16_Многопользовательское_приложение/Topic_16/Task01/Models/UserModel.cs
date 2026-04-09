c6c159890a13ae583c45c9cd4d6b1646e943f4b7
pu2toy <de.fan563gho@gmail.com>
2026-04-09 13:30:17 +0300

    Topic_17

---
+++ 16_Многопользовательское_приложение/Topic_16/Task01/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01.Models
+{
+    public class UserModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } 
+    }
+}
---
+++ 17_Работа_с_анимацией/Topic_17/Task01/Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01.Models
+{
+    public class OrderModel
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }  
+        public ClientModel client { get; set; }
+    }
+}
